cc84adfabb16620b1c4005b7e823f29202526156
Harshad Thorat <harshad16042004@gmail.com>
2025-08-18 20:58:46 +0530

    Updated FloatingTabBar.tsx

--- components/FloatingTabBar.tsx
+++ components/FloatingTabBar.tsx
@@ -7,7 +7,7 @@ import Animated, {
   withTiming,
   interpolate,
   runOnJS,
-} from 'react-native-reanimated';
+} from 'react-native-worklets';
 import { LinearGradient } from 'expo-linear-gradient';
 import { BlurView } from 'expo-blur';
 import * as Haptics from 'expo-haptics';
