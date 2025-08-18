0a0381bd0dd2adf181ed40ae6c0befa5931ebe25
Harshad Thorat <harshad16042004@gmail.com>
2025-08-18 20:59:38 +0530

    Updated ScanningOverlay.tsx

--- components/ScanningOverlay.tsx
+++ components/ScanningOverlay.tsx
@@ -7,7 +7,7 @@ import Animated, {
   withTiming,
   interpolate,
   Easing,
-} from 'react-native-reanimated';
+} from 'react-native-worklets';
 import { LinearGradient } from 'expo-linear-gradient';
 
 interface ScanningOverlayProps {
